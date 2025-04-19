05cf95cc4852a1af8206e6518b18c91744732a80
gdstchdr1 <72061401+gdstchdr1@users.noreply.github.com>
2025-04-19 22:40:21 +0800

    Update rename-luci-menu.sh

--- patch/luci-i18n/rename-luci-menu.sh
+++ patch/luci-i18n/rename-luci-menu.sh
@@ -14,4 +14,8 @@ sed -i 's/msgid "DDNS-Go"/msgid "DDNS-Go"\nmsgstr "动态域名"/' \
 sed -i 's/msgid "Vlmcsd KMS 服务器"/msgid "Vlmcsd KMS 服务器"\nmsgstr "KMS 激活"/' \
     feeds/luci/applications/luci-app-vlmcsd/po/zh_Hans/vlmcsd.po
 
+# 修改 acme 菜单项
+sed -i 's/msgid "ACME"/msgid "ACME"\nmsgstr "证书申请"/' \
+    feeds/luci/applications/luci-app-acme/po/zh_Hans/acme.po
+
 echo "[+] LuCI 菜单名称已修改完成。"
